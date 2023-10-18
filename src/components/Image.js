38d55a6e26508605ed3343741e5aec1165e547e3
maradabhargavnaidu <bhargav.naidu.mbn.18@gmail.com>
2023-10-18 23:29:29 +0530

    update responsive

--- src/components/Image.js
+++ src/components/Image.js
@@ -28,40 +28,44 @@ const Image = () => {
     <div className="dark:bg-gray-800">
       {t && (
         <div className="container mx-auto">
-          <div className="flex items-center justify-center">
-            <div className="flex flex-col">
-              <img src={imgdata.urls.raw} />
-              <div className="flex justify-between my-5">
-                <div className="flex gap-3">
-                  <img
-                    src={imgdata.urls.raw}
-                    className="w-10 h-10 rounded-full"
-                  />
-                  <div>
-                    <div className="dark:text-white">{imgdata.user.name}</div>
-                    <div className="dark:text-white">@{imgdata.user.name}</div>
-                  </div>
-                </div>
-                <div className="flex items-center gap-6">
-                  <div>
-                    <div className="dark:text-white">
-                      {imgdata.downloads} downloads
+          <div className="">
+            <div className="flex items-center justify-center">
+              <div className="flex flex-col">
+                <img src={imgdata.urls.full} className="w-auto h-[800px]" />
+                <div className="flex justify-between my-5">
+                  <div className="flex gap-3">
+                    <img
+                      src={imgdata.urls.full}
+                      className="w-10 h-10 rounded-full"
+                    />
+                    <div>
+                      <div className="dark:text-white">{imgdata.user.name}</div>
+                      <div className="dark:text-white">
+                        @{imgdata.user.name}
+                      </div>
                     </div>
                   </div>
-                  <div className="flex gap-2 items-center dark:text-white">
-                    <i class="fa-regular fa-thumbs-up text-3xl"></i>
-                    <div className="dark:text-white">{imgdata.likes}</div>
+                  <div className="flex items-center gap-6">
+                    <div>
+                      <div className="dark:text-white">
+                        {imgdata.downloads} downloads
+                      </div>
+                    </div>
+                    <div className="flex gap-2 items-center dark:text-white">
+                      <i class="fa-regular fa-thumbs-up text-3xl"></i>
+                      <div className="dark:text-white">{imgdata.likes}</div>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div>
-                <div className="dark:text-white">Related Tags</div>
-                <div className="flex gap-3 my-3">
-                  {imgdata.tags.map((tags) => (
-                    <div className="dark:text-white dark:bg-gray-700 bg-gray-300 p-2 rounded-md">
-                      {tags.type}
-                    </div>
-                  ))}
+                <div>
+                  <div className="dark:text-white">Related Tags</div>
+                  <div className="flex flex-wrap gap-3 my-3">
+                    {imgdata.tags.map((tags) => (
+                      <div className="dark:text-white dark:bg-gray-700 bg-gray-300 p-2 rounded-md">
+                        {tags.type}
+                      </div>
+                    ))}
+                  </div>
                 </div>
               </div>
             </div>
